fb01b353329ccd3ee746684fb4972d67b1000068
W <h@Ws-MacBook-Pro.local>
2019-09-03 14:28:09 -0700

    Update story to remove typo

--- mad_libs.py
+++ mad_libs.py
@@ -10,7 +10,7 @@ aan_mode: bool = False # Toggle aan_identifier generation //EXPERIMENTAL//
 rand_words: bool = False 
 
     
-# Pls no modify below #
+# Pls no modify below # 
 
 
 
@@ -165,7 +165,7 @@ def story_input_loops():
                 
             else:
                 print ('Please enter another ' + slot_desc.display_name)
-                
+            
             user_input = ask_for_input(False)
             
             if slot_desc.aan_place_flag:
@@ -239,7 +239,7 @@ def test_story_init():
     slot_list_test.append(SlotOfSpeech("past tense verb", "#verb_3", 2, 'a', False))
     slot_list_test.append(SlotOfSpeech("plural noun", "#noun_2", 1, 'a', False))
 
-    test_story = "There once was tall, #adjective_1 man, by the name of #person_name_1. One day, while #verb_2 near #place_name_1, he saw a massive #noun_1! It was nearly the size of ten #noun_2. \"I have to tell #person_name_1 about this\" he thought, as he #verb_3 there. He #adverb_1 took out his #noun_1 and #verb_3! Having done that, he ran home -to #place_name_1- where he he could finnaly #verb_1."
+    test_story = "There once was tall, #adjective_1 man, by the name of #person_name_1. One day, while #verb_2 near #place_name_1, he saw a massive #noun_1! It was nearly the size of ten #noun_2. \"I have to tell #person_name_1 about this\" he thought, as he #verb_3 there. He #adverb_1 took out his #noun_1 and #verb_3! Having done that, he ran home -to #place_name_1- where he could finnaly #verb_1."
     #verb_2 in #place_name_1, when he spotted #animal_1"
     
     return MadlibFile(test_story, slot_list_test)
